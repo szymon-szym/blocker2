2f720d33b9d416e6f872284e725bb4fca085c264
szymon <szymon.mich@gmail.com>
2020-08-16 22:24:59 +0200

    test for pushing logs to db

--- tests/unit/content.spec.js
+++ tests/unit/content.spec.js
@@ -5,16 +5,18 @@ describe('Logging to db',  () => {
   global.fetch = jest.fn(() => {
     Promise.resolve("ok")
   })
-  global.browser = jest.fn(() => {
-    Promise.resolve("ok")
-  })
+  
+  const url = 'https://yqibl4m4yj.execute-api.us-west-2.amazonaws.com/prod/logs/'
   const quantity = 12
   const token = 'ABC'
 
   const res = await postAddsLogs(quantity, token)
 
-  expect(fetch).toBeCalledTimes(2)
-  expect(fetch).toBeCalledWith('ABC')
+  expect(fetch).toBeCalledTimes(1)
+  expect(fetch).toBeCalledWith(url, expect.anything())
+  expect(fetch).toBeCalledWith(expect.anything(), expect.objectContaining({headers: {
+    Authorization: token
+  }}))
 
   })
 })
